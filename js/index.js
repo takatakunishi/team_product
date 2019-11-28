e19b335e96f947b3bd47de75034842988bf9acdf
b1018036 <basuktutom@gmail.com>
2019-11-28 13:06:30 +0900

    [fix]再帰を修正した。

--- js/index.js
+++ js/index.js
@@ -4,8 +4,6 @@
   let boxNumber = 64;
   let mine_number = 10; //爆弾の個数
   let p_array = [];
-  let field = new Map(); //Mapの作成
-  let mine = new Map(); //Mapの作成
   let MINE = [];
   let FIELD = [];
   let t = true;
@@ -28,24 +26,24 @@
         console.log('Place:' + Place);
 
         console.log('for start');
-        for(var i = 0; i < 64; i++){
-          field.set(i , t); //falseが爆弾
-          mine.set(i, 0); //iは位置０は周囲に存在する爆弾の個数
-        } //Mapを設定
         //for文で要素を格納する
         for(var n = 0; n < 8; n++){
           FIELD[n] = [];
+          MINE[n] = [];
           for(var m = 0; m < 8; m++){
-            FIELD[n][m] = field.get(n*m);
+            FIELD[n][m] = true;//falseが爆弾
+            MINE[n][m] = 0;
           }
         }
         console.log('for end');
+        console.log(MINE);
+        console.log(FIELD);
 
-        /** 重複チェックしながら乱数作成 */
-        for(i = 0; i < mine_number; i++){
+        /** 重複チェックしながら乱数作成 */ 
+        for(i = 0; i < mine_number; i++){//<-ここを修正
           while(true){
             var tmp = intRandom(min, max);
-            if(!randoms.includes(tmp) && tmp != Place){
+            if(!randoms.includes(tmp) && tmp != Place && tmp != Place-1 && tmp != Place-7 && tmp != Place-8 && tmp != Place-9 && tmp != Place+1 && tmp != Place+7 && tmp != Place+8 && tmp != Place+9){
               randoms.push(tmp);
               break;
             }
@@ -56,88 +54,83 @@
         for(var i = 0; i < 64; i++){
           if(randoms.includes(i)){
             //for文で要素を格納する
-            field.set(i, f);
             for(var n = 0; n < 8; n++){
               for(var m = 0; m < 8; m++){
                 console.log('ok');
                 if((n*8)+m == i){
                   FIELD[n][m] = false;
+                  MINE[n][m] = -1;//爆弾設置
                   if(n-1 >= 0 && m-1 >= 0 && FIELD[n-1][m-1]){
                     console.log('1');
-                    var a = (n-1)*8+(m-1);
-                    var b = Number(mine.get(a));
-                    mine.set(a,b+1);
+                    var b = Number(MINE[n-1][m-1]);
+                    MINE[n-1][m-1] = b+1;
                   }
                   if(n-1 >= 0 && FIELD[n-1][m]){
                     console.log('2');
-                    var a = (n-1)*8+m;
-                    var b = Number(mine.get(a));
-                    mine.set(a,b+1);
+                    var b = Number(MINE[n-1][m]);
+                    MINE[n-1][m] = b+1;
                   }
                   if(n-1 >= 0 && m+1 < 8 && FIELD[n-1][m+1]){
                     console.log('3');
-                    var a = (n-1)*8+(m+1);
-                    var b = Number(mine.get(a));
-                    mine.set(a,b+1);
+                    var b = Number(MINE[n-1][m+1]);
+                    MINE[n-1][m+1] = b+1;
                   }
                   if(m+1 < 8 && FIELD[n][m+1]){
                     console.log('4');
-                    var a = n*8+(m+1);
-                    var b = Number(mine.get(a));
-                    mine.set(a,b+1);
+                    var b = Number(MINE[n][m+1]);
+                    MINE[n][m+1] = b+1;
                   }
                   if(n+1 < 8 && m+1 < 8 && FIELD[n+1][m+1]){
                     console.log('5');
-                    var a = (n+1)*8+(m+1);
-                    var b = Number(mine.get(a));
-                    mine.set(a,b+1);
+                    var b = Number(MINE[n+1][m+1]);
+                    MINE[n+1][m+1] = b+1;
                   }
                   if(n+1 < 8 && m-1 >= 0 && FIELD[n+1][m-1]){
                     console.log('6');
-                    var a = (n+1)*8+(m-1);
-                    var b = Number(mine.get(a));
-                    mine.set(a,b+1);
+                    var b = Number(MINE[n+1][m-1]);
+                    MINE[n+1][m-1] = b+1;
                   }
                   if(n+1 < 8 && FIELD[n+1][m]){
                     console.log('7');
-                    var a = (n+1)*8+(m);
-                    var b = Number(mine.get(a));
-                    mine.set(a,b+1);
+                    var b = Number(MINE[n+1][m]);
+                    MINE[n+1][m] = b+1;
                   }
                   if(m-1 >= 0 && FIELD[n][m-1]){
                     console.log('8');
-                    var a = n*8+(m-1);
-                    var b = Number(mine.get(a));
-                    mine.set(a,b+1);
+                    var b = Number(MINE[n][m-1]);
+                    MINE[n][m-1] = b+1;
                   }
                 }
               }
             } 
-            mine.set(i, -1); //爆弾設置
-            console.log(mine);
           }
         }
-        //for文で要素を格納する
         for(var n = 0; n < 8; n++){
-          MINE[n] = [];
           for(var m = 0; m < 8; m++){
-            MINE[n][m] = mine.get((n*8)+m);
+            if(Place == (n*8)+m){
+              mine_check(n, m);
+            }
           }
         }
-        mine_check(Place);
       }else{
-        console.log(mine.get(Place));//undefinedになる <- 何故？
-        if(field.get(Place)){//改良が必要
-          console.log('safe1');
-          mine_check(Place);
-        }else{
-          /*ゲームオーバーの処理*/
-          const loose = document.getElementById('loose');
-          loose.classList.add('loose');
-          loose.classList.remove('none');
-          console.log('game 0ver');
+        for(var n = 0; n < 8; n++){
+          for(var m = 0; m < 8; m++){
+            if(Place==(n*8)+m){
+              if(FIELD[n][m]){//改良が必要
+                console.log('safe1');
+                mine_check(n, m);
+              }else{
+                /*ゲームオーバーの処理*/
+                const loose = document.getElementById('loose');
+                loose.classList.add('loose');
+                loose.classList.remove('none');
+                console.log('game 0ver');
+              }
+            }
+          }
         }
       }
+      //console.log(MINE);
       this.classList.add('safe');
       boxNumber--;
       if(mine_number == boxNumber){
@@ -149,32 +142,33 @@
     box.setAttribute("id",idname);
   }
 
-  function mine_check(p){
-    if(p >= 0 && p < 64){
-      if(!p_array.includes(p)){
-        p_array.push(p);
+  function mine_check(n, m){
+    if((n >= 0 && m >= 0) && (n < 8 && m < 8)){
+      if(!p_array.includes((n*8)+m)){
+        p_array.push((n*8)+m);
         for(var i=0; i<8; i++){
           for(var j=0; j<8; j++){
-            if(p == i*8+j){
+            if(n == i && m == j){
               if(MINE[i][j] == 0){
               /*周囲８マスをクリックしたことにしたい*/
-              const box = document.getElementById(p);
+                mine_check(n, m);
+                mine_check(n, m-1);
+                mine_check(n-1, m+1);
+                mine_check(n-1, m);
+                mine_check(n-1, m-1);
+                mine_check(n, m+1);
+                mine_check(n+1, m+1);
+                mine_check(n+1, m);
+                mine_check(n+1, m-1);
+                const box = document.getElementById((n*8)+m);
                 box.classList.add('safe');
-                mine_check(p-1);
-                mine_check(p-7);
-                mine_check(p-8);
-                mine_check(p-9);
-                mine_check(p+1);
-                mine_check(p+7);
-                mine_check(p+8);
-                mine_check(p+9);
               }else if(MINE[i][j] > 0){
-              const box = document.getElementById(p);
+                const box = document.getElementById((n*8)+m);
                 box.classList.add('safe');
-                let squareNumber = mine.get(p);
+                let squareNumber = MINE[n][m];
               }else{
                 console.log('stopping');
-                let squareNumber = mine.get(p);
+                let squareNumber = MINE[n][m];
                 console.log('p_array:' + p_array);
                 /*マスに数字を表示する処理*/
               }
